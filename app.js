52c739153cf3c84515840c8d63962783ff4bed61
Touhidul Islam Chayan <imchn24@gmail.com>
2020-08-18 07:35:26 +0600

    app.js added port env

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
 var request = require('request');
 var express = require('express');
-const { response } = require('express');
-const { render } = require('ejs');
+const {
+    response
+} = require('express');
+const {
+    render
+} = require('ejs');
+const port = process.env.PORT || 8080;
 var app = express();
 app.use(express.static('public'));
 app.set("view engine", "ejs");
 
-app.get("/", function(req, res){
-        res.render("search");
+app.get("/", function (req, res) {
+    res.render("search");
 });
 
 
-app.get("/results", function(req, res){
+app.get("/results", function (req, res) {
     var query = req.query.search;
-    var url = "http://www.omdbapi.com/?t="+query+"&apikey=1b41c4c1";
-     request(url,function(error,response,body){
-         if(!error && response.statusCode==200){
-            var data = JSON.parse(body); 
-            res.render("results", {data:data});    
+    var url = "http://www.omdbapi.com/?t=" + query + "&apikey=1b41c4c1";
+    request(url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var data = JSON.parse(body);
+            res.render("results", {
+                data: data
+            });
         }
     });
 });
@@ -25,4 +32,4 @@ app.get("/results", function(req, res){
 
 
 
-app.listen(8080,()=>console.log("Movie app started"));
+app.listen(port, () => console.log("Movie app started"));
