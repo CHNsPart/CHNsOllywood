11d38a0c346c64a51037b859c59c1d36b76c0ef5
Touhidul Islam Chayan <imchn24@gmail.com>
2020-08-18 09:01:09 +0600

    flash added

--- app.js
+++ app.js
@@ -1,35 +1,53 @@
-var request = require('request');
-var express = require('express');
+var request = require("request");
+var flash = require("connect-flash");
+const session = require("express-session");
+var express = require("express");
 const {
     response
-} = require('express');
+} = require("express");
 const {
     render
-} = require('ejs');
+} = require("ejs");
 const port = process.env.PORT || 8080;
 var app = express();
-app.use(express.static('public'));
+app.use(express.static("public"));
+app.use(
+    session({
+        secret: "chnsollywood",
+        saveUninitialized: true,
+        resave: true,
+    })
+);
+app.use(flash());
+
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-    res.render("search");
-});
-
-
-app.get("/results", function (req, res) {
-    var query = req.query.search;
-    var url = "http://www.omdbapi.com/?t=" + query + "&apikey=1b41c4c1";
-    request(url, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var data = JSON.parse(body);
-            res.render("results", {
-                data: data
-            });
-        }
+    res.render("search", {
+        message: req.flash('error')
     });
 });
 
-
-
+app.get("/results", function (req, res) {
+    var query = req.query.search;
+    if (query.length === 0) {
+        req.flash("error", "Sorry, Couldn't find your Movie Search!");
+        res.redirect("/");
+    } else {
+        var url = "http://www.omdbapi.com/?t=" + query + "&apikey=1b41c4c1";
+        request(url, function (error, response, body) {
+            if (error) {
+                req.flash("error", "Sorry, Couldn't find your Movie Search!");
+                res.redirect("/");
+            }
+            if (!error && response.statusCode == 200) {
+                var data = JSON.parse(body);
+                res.render("results", {
+                    data: data,
+                });
+            }
+        });
+    }
+});
 
 app.listen(port, () => console.log("Movie app started"));
